4488002da0b91b8b821d92a535b38e97353280a9
muditgoel135 <muditgoel135@gmail.com>
2026-07-26 13:15:24 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host=os.getenv("HOST"), port=int(os.getenv("PORT")))
